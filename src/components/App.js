ba64695c497cb14f67734956a1c65ed8c0ee3e41
Arkadiusz Milewski <asmilewski@gmail.com>
2021-01-06 15:35:05 +0100

    webpack issue solved

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Searchbar from "./SearchBar";
 const App = () => {
   return (
-    <div>
+    <div className="ui container" style={{ marginTop: "10px" }}>
       <Searchbar />
     </div>
   );
