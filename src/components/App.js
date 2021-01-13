1f19c79b5f6666d6a14d4f6640b84eeaa7dc7542
Arkadiusz Milewski <asmilewski@gmail.com>
2021-01-13 10:49:44 +0100

    invoking call back between App and searchBar components done

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Searchbar from "./SearchBar";
 class App extends React.Component {
+  onSearchSubmit(term) {
+    console.log(term);
+  }
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        <Searchbar />
+        <Searchbar onSubmit={this.onSearchSubmit} />
       </div>
     );
   }
